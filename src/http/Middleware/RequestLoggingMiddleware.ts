49eab0e1ad675bf0ee3a573b053f264a2446b4dc
Prakash Tank <prakashtank106@gmail.com>
2026-02-14 23:46:26 +0530

    Fix: Call method() and path() correctly in Logging Middleware

--- src/http/Middleware/RequestLoggingMiddleware.ts
+++ src/http/Middleware/RequestLoggingMiddleware.ts
@@ -4,7 +4,8 @@ import { Log } from '@arikajs/logging';
 export class RequestLoggingMiddleware {
     public async handle(request: any, next: (request: any) => Promise<any>): Promise<any> {
         const start = Date.now();
-        const { method, url } = request;
+        const method = request.method();
+        const url = request.path();
 
         const response = await next(request);
 
